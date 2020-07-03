e857b5ca410ab6c29adaf14a052c00c4a899880c
Juan Estrella <juan.estrella137@gmail.com>
2020-07-03 17:48:58 +0200

    Inital commit, create nls command for cli to list all files/dirs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi there!');
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+fs.readdir(process.cwd(), (err, filenames) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  console.log(filenames);
+});
