f5dfdfb740a39eb6c077423d04f5ddef87c652c3
Juan Estrella <juan.estrella137@gmail.com>
2020-07-05 19:12:14 +0200

    Check if file or folder

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const { lstat } = fs.promises;
+const chalk = require('chalk');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     throw new Error(err);
   }
 
-  console.log(filenames);
+  const statPromises = filenames.map((filename) => {
+    return lstat(filename);
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    }
+    else {
+      console.log(chalk.green.bold(filenames[index]));
+    }
+  }
 });
